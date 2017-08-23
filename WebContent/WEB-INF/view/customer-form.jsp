64d2ef270930cc3cfc229d89ba43816e201aeb91
George Nguyen <g.nguyen92@yahoo.com>
2017-08-22 17:33:11 -0700

    Add zipcode field @Pattern regex form validation

--- WebContent/WEB-INF/view/customer-form.jsp
+++ WebContent/WEB-INF/view/customer-form.jsp
@@ -21,7 +21,13 @@
 		Last Name*: <form:input path="lastName" />
 		<form:errors path="lastName" cssClass="error" /><br><br>
 		Password*:  <form:password path="password" />
-		<form:errors path="password" cssClass="error" /><br><br>
+		<form:errors path="password" cssClass="error" /><br><br><br>
+		
+		Street Address: <form:input path="address" /><br><br>
+		Zip Code*: <form:input path="zipcode" />
+		<form:errors path="zipcode" cssClass="error" /><br><br>
+		
+		
 		
 		<input type="submit" value="Login" />
 	</form:form>
--- src/com/gnguyen92/springmvc/Customer.java
+++ src/com/gnguyen92/springmvc/Customer.java
@@ -1,6 +1,7 @@
 package com.gnguyen92.springmvc;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 /* 
  * 	Form Validation using Spring annotations
  *  @NotNull(message="message to display")
@@ -11,15 +12,26 @@ import javax.validation.constraints.Size;
 
 public class Customer {
 
-	@NotNull(message="is required.")
-	@Size(min=2, message="First name must be at least 2 characters long.")
+	// @NotNull specifies a required field
+	@NotNull(message=" is required.")
+	@Size(min=2, message=" must be at least 2 characters long.")
 	private String firstName;
-	@NotNull(message="is required.")
-	@Size(min=2, message="Last name must be at least 2 characters long.")
+	
+	@NotNull(message=" is required.")
+	@Size(min=2, message=" must be at least 2 characters long.")
 	private String lastName;
-	@NotNull(message="is required.")
-	@Size(min=2, message="Password must be at least 2 characters long.")
+	
+	@NotNull(message=" is required.")
+	@Size(min=2, message=" must be at least 2 characters long.")
 	private String password;
+	
+	private String address;
+	
+	// @Pattern forces field to follow a regex pattern
+	// regular expression code: only digits 0-9 allowed.  Only 5 characters allowed. [a-zA-Z] only alphabet allowed
+	@NotNull(message=" is required.")
+	@Pattern(regexp="[0-9]{5}", message=" invalid Zip Code.")
+	private String zipcode;
 	
 	
 	// GETTERS & SETTERS
@@ -40,6 +52,18 @@ public class Customer {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
 	}
 	
 	
